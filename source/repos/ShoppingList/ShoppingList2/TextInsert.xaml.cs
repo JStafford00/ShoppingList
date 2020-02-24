83c3ef9a400b301dab55fd3037eb0084cd297701
Jordan Stafford <jstafford00@ksu.edu>
2020-02-24 15:29:55 -0600

    Done

---
+++ source/repos/ShoppingList/ShoppingList2/TextInsert.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ShoppingList2
+{
+    /// <summary>
+    /// Interaction logic for TextInsert.xaml
+    /// </summary>
+    public partial class TextInsert : UserControl
+    {
+        public TextInsert()
+        {
+            InitializeComponent();
+        }
+
+        private void OnItemAddButtonClicked(object sender, RoutedEventArgs e)
+        {
+            //var data = DataContext as ShoppingListData;
+
+            if(DataContext is ShoppingListData data)
+            {
+                data.Add(ItemsToAdd.Text);
+            }
+        }
+    }
+}
